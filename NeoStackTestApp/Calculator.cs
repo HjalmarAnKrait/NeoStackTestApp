68b5e7ac61c0eac755f26ba0ea1daf098260ff46
Tarabrin Egor <tarabrin2142@gmail.com>
2021-11-18 12:32:04 +0300

    Добавлена логика работы калькулятора и написаны Unit-тесты для его проверки.

---
+++ NeoStackTestApp/Calculator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeoStackTestApp
+{
+    public static class Calculator
+    {
+
+        public static double CalculateFunction(double degree, FunctionCalcModel functionCalcModel)
+        {
+            if (degree == 0)
+                degree = 1;
+
+            double result =
+                functionCalcModel.A * Math.Pow(functionCalcModel.X, degree) + 
+                functionCalcModel.B * Math.Pow(functionCalcModel.Y, degree - 1.0) +
+                CalcC(degree, functionCalcModel.C);
+
+            return result;
+        }
+
+        public static double CalculateFunction(FunctionCalcModel functionCalcModel)
+        {
+            if (functionCalcModel.Degree == 0)
+                functionCalcModel.Degree = 1;
+
+            double result =
+                functionCalcModel.A * Math.Pow(functionCalcModel.X, functionCalcModel.Degree) +
+                functionCalcModel.B * Math.Pow(functionCalcModel.Y, functionCalcModel.Degree - 1.0) +
+                CalcC(functionCalcModel.Degree, functionCalcModel.C);
+
+            return result;
+        }
+
+        public static double CalcC(double degree, double c)
+        {
+            if (degree == 0)
+                degree = 1;
+
+            double result = Math.Pow(10, degree - 1.0) * c;
+
+            return result;
+        }
+
+        public static List<double> GetCList(int size, double degree)
+        {
+            List<double> result = new List<double>();
+
+            for (int i = 1; i <= size; i++)
+            {
+                result.Add(CalcC(degree, i));
+            }
+
+            return result;
+        }
+
+       
+   
+
+        
+    }
+}
---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+using NeoStackTestApp;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        
+
+        [TestMethod]
+        public void CalcCTest()
+        {  
+            Assert.AreEqual(40, Calculator.CalcC(2, 4));
+            Assert.AreEqual(10, Calculator.CalcC(2, 1));
+            Assert.AreEqual(1, Calculator.CalcC(1, 1));
+            Assert.AreEqual(4, Calculator.CalcC(1, 4));
+            Assert.AreEqual(0, Calculator.CalcC(0, 0));
+        }
+
+        [TestMethod]
+        public void GetCListTest()
+        {
+            Assert.AreEqual(5, Calculator.GetCList(5, 1).Count);
+            Assert.AreEqual(153, Calculator.GetCList(153, 1).Count);
+            Assert.AreEqual(0, Calculator.GetCList(0, 1).Count);
+            Assert.AreEqual(0, Calculator.GetCList(-143, 1).Count);
+        }
+
+        [TestMethod]
+        public void CalculateFunctionTest()
+        {
+            FunctionCalcModel functionCalcModelZero = new FunctionCalcModel();
+            FunctionCalcModel functionCalcModel = new FunctionCalcModel(1,1,1,1,1,1,2);
+
+            Assert.AreEqual(0.0, Calculator.CalculateFunction(0.0, functionCalcModelZero));
+            Assert.AreEqual(12.0, Calculator.CalculateFunction(functionCalcModel));
+        }
+    }
+}
