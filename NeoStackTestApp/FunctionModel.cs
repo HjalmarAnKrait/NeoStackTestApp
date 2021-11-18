15c236c79265c2ac1eb3fb5c1954d2652598a1a3
Tarabrin Egor <tarabrin2142@gmail.com>
2021-11-18 09:48:46 +0300

    Свёрстан интерфейс

---
+++ NeoStackTestApp/FunctionModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeoStackTestApp
+{
+    class FunctionModel
+    {
+        public string FunctionName { get;}
+        public string Function { get;}
+        public int CMultiplier { get;}
+        public int Degree { get; }
+
+        //private int 
+
+        public int getMultipliedC(int c)
+        {
+            return c * CMultiplier;
+        }
+
+        public string getFunctionDescription()
+        {
+            string functionDescription = $"Выбранная функция:\n{FunctionName} {Function}";
+            return functionDescription;
+        }
+    }
+}
