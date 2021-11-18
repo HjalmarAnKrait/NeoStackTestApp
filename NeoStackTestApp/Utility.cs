503aeb52a62223be80928102e2401ee495eab496
Tarabrin Egor <tarabrin2142@gmail.com>
2021-11-18 09:48:46 +0300

    Свёрстан интерфейс

---
+++ NeoStackTestApp/Utility.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeoStackTestApp
+{
+    internal static class Utility
+    {
+       static Boolean checkIsValid(String inputStr) 
+        {
+            Boolean isValid = false;
+
+            //TODO: проверка строки
+
+            return isValid;
+        }
+    }
+}
